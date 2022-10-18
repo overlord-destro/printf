f425d404e33562bf635eec37d7c5c338b7f266a4
overlord-destro <edemcoffie41@gmail.com>
2022-10-18 02:32:45 -0700

    Added printf function

---
+++ _printf.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*check_format_specifier - checks for valid format specifier
+*@format: format specifier
+*Return: pointer to function or null
+*/
+static int (*check_format_specifiers(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{NULL, NULL}
+	};
+
+	for (i = 0; p[i] != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
+
+/**
+*_printf - prints anything given the format
+*@format: arguments to the function
+*Return: number of characters printed
+*/
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	va_start(va_list, format)
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = check_format_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
+	return (count);
+}
