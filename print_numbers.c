2d4eb10bc333f74dc29cdbd49612f208e4a14157
overlord-destro <edemcoffie41@gmail.com>
2022-10-18 08:17:03 -0700

    .

--- print_numbers.c
+++ print_numbers.c
@@ -7,7 +7,7 @@
 int print_i(va_list i)
 {
 	int a[10];
-	int i, m, n, sum, count;
+	int j, m, n, sum, count;
 
 	n = va_arg(i, int);
 	count = 0;
@@ -22,7 +22,7 @@ int print_i(va_list i)
 	{
 		_putchar('-');
 		count++;
-		for (j = 0, j < 10; j++)
+		for (j = 0; j < 10; j++)
 			a[j] *= -1;
 	}
 	for (j = 0, sum = 0; j < 10; j++)
