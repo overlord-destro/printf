fe2bafbb39a0ef9565bc381962e53cfee30e8483
missgevan <leviagevansekyi@gmail.com>
2022-10-18 07:31:02 -0700

    Added header files

--- main.h
+++ main.h
@@ -3,8 +3,18 @@
 
 #include <stdarg.h>
 
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
 int _printf(const char *format, ...);
 int _putchar(char);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
 
 
 
